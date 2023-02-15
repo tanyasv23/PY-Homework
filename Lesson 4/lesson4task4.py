b6b2c8ab963abd9dc2ffef6cb8431acb9c1fa40f
tanya.sv <tan.ua.vv@gmail.com>
2023-02-15 16:38:56 +0200

    updated

--- Lesson 4/lesson4task4.py
+++ Lesson 4/lesson4task4.py
@@ -1,16 +1,26 @@
 name = "tanya"
-inp = input("Please enter your name ")
+inp = input("Please enter your name ") # Tanya
 nameLen = len(name)
 inpLen = len(inp)
-i = 0
 
+#v1
 if nameLen != inpLen:
-    print (False)
+    print ("V1 Result: ", False)
 else:
     if name.lower() == inp.lower():
-        print (True)
+        print ("V1 Result: ", True)
     else:
-        print (False)
+        print ("V1 Result: ", False)
 
- 
-     
+#v2
+i = 0
+if nameLen == inpLen:
+    while i < nameLen:
+        if name[i].upper() != inp[i].upper():
+            print("V2 Result: ", False)
+            break
+        elif i == nameLen - 1:
+            print ("V2 Result: ", True)
+        i += 1
+else:
+    print("V2 Result: ", False)
